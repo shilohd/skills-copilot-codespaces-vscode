8e11dc67d00d0acc3546a84d9f35e69fcf22522c
shilohd <57630783+shilohd@users.noreply.github.com>
2024-11-20 17:02:01 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,13 @@
+function skillsMember() {
+  var member = [
+    {
+      name: "Dino",
+      skills: ["HTML", "CSS", "JS"]
+    },
+    {
+      name: "Sisi",
+      skills: ["HTML", "JS"]
+    }
+  ];
+  return member;
+}
