ee658d2ce62e1e778e49e016d243125e043dc4fb
shilohd <57630783+shilohd@users.noreply.github.com>
2024-11-20 17:18:58 +0000

    Copilot fourth commit

---
+++ comments.js
@@ -0,0 +1,7 @@
+//create web server 
+//create web server 
+var express = require('express');
+var app = express();
+app.use(express.static(__dirname + '/public'));
+app.listen(8000);
+console.log('Server running at http://
